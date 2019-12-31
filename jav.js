69b99640effcd32cd45cd5c60c6ced6aabd84efd
Blaise <ifollowjre@gmail.com>
2019-12-30 19:22:55 -0700

    Added console logs to see what i was grabbing from the api at what day and time, also changed the display of the moment js

--- jav.js
+++ jav.js
@@ -3,21 +3,56 @@ $(document).ready(function () {
     function theSearch() {
         var city = $("#searchBar").val();
         var request = new XMLHttpRequest()
-        request.open('GET', "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=44c38455c07369969dfcf31cc60c6f41&units=imperial", true)
+        request.open('GET', "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=44c38455c07369969dfcf31cc60c6f41&units=imperial", true)
         request.onload = function () {
             // Begin accessing JSON data here
             var res = JSON.parse(this.response)
             if (request.status >= 200 && request.status < 400) {
-                var temp = (res.main.temp);
-                var humid = (res.main.humidity);
-                var windSpeed = (res.wind.speed);
-                // var tempa = parseInt(temp − 273.15) × 9/5 + 32;
+                var temp1 = (res.list[3].main.temp + '°F');
+                var temp2 = (res.list[3].main.temp + '°F');
+                var temp3 = (res.list[3].main.temp + '°F');
+                var temp4 = (res.list[3].main.temp + '°F');
+                var temp5 = (res.list[3].main.temp + '°F');
+
+
+                var humid1 = ('Humidity ' + res.list[3].main.humidity + '%');
+                var humid2 = ('Humidity ' + res.list[11].main.humidity + '%');
+                var humid3 = ('Humidity ' + res.list[19].main.humidity + '%');
+                var humid4 = ('Humidity ' + res.list[27].main.humidity + '%');
+                var humid5 = ('Humidity ' + res.list[35].main.humidity + '%');
+
+                var wind1 = ('Wind Speed ' + res.list[3].wind.speed + 'Mph')
+                var wind2 = ('Wind Speed ' + res.list[11].wind.speed + 'Mph')
+                var wind3 = ('Wind Speed ' + res.list[19].wind.speed + 'Mph')
+                var wind4 = ('Wind Speed ' + res.list[27].wind.speed + 'Mph')
+                var wind5 = ('Wind Speed ' + res.list[35].wind.speed + 'Mph')
+                // var windSpeed = (res.wind.speed);
                 console.log(res);
-                console.log(res.name);
-                // console.log(res);
-                console.log('Temperature ' + temp + '°F');
-                console.log('Humidity ' + humid + '%');
-                console.log('Wind Speed ' + windSpeed + ' Mph');
+                console.log(res.list[3]);
+                console.log(temp1);
+                console.log(humid1);
+                console.log(wind1);
+
+                console.log(res.list[11]);
+                console.log(temp2);
+                console.log(humid2);
+                console.log(wind2);
+
+                console.log(res.list[19]);
+                console.log(temp3);
+                console.log(humid3);
+                console.log(wind3);
+
+                console.log(res.list[27]);
+                console.log(temp4);
+                console.log(humid4);
+                console.log(wind4);
+
+                console.log(res.list[35]);
+                console.log(temp5);
+                console.log(humid5);
+                console.log(wind5);
+
 
             } else {
                 console.log('error')
@@ -27,8 +62,12 @@ $(document).ready(function () {
 
     }
 
-
-    // $('#searchBar').bind("enterKey", function (e) { });
+    ////// THE ON CLICK OR ENTER BUTTON FUNCTIONS  /////
+    ////////                               //////
+    /////////                   ////////
+    ///////             ///////
+    //////      ///////
+    /////////
     $('#searchBar').keyup(function (e) {
         if (e.keyCode == 13) {
             $(this).trigger("enterKey");
@@ -41,8 +80,12 @@ $(document).ready(function () {
         console.log('You clicked the submit button')
         theSearch();
     });
-
-
+    /////
+    /////      /////////
+    ///////               ///////////
+    ///////                         /////////
+    ///////                                 /////////
+    ////// THE ON CLICK OR ENTER BUTTON FUNCTIONS  /////
 
 
 
--- moment.js
+++ moment.js
@@ -2,7 +2,7 @@ const realTime = document.getElementById("theTime")
 
 setInterval(() => {
     const now = moment()
-    const theTime = now.format('MMMM dddd, Do, GGGG')
+    const theTime = now.format('MMMM dddd, Do, GGGG, HH:mm:s ')
     // console.log(theTime);
     realTime.textContent = theTime;
 }, 1000);
